aaaa672ddfa83a33fc2f238e6b35ecb9691cea3c
Liam <davisliam123@gmail.com>
2024-02-12 19:52:55 -0500

    wrote setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+dist = setup(
+    name="credit card customer segmentation",
+    author="Liam Davis",
+    license="MIT",
+    url="https://github.com/liamjdavis/Credit-Card-Customer-Segmentation.git",
+    packages="credit_card_customer_segmentation",
+)
